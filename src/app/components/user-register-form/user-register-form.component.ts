27db41018151a0409f970e4421af91199b1ca9b4
Brian Thomas <btdeveloper125@gmail.com>
2022-01-24 11:24:37 -0700

    refactor(User Registration): Connect api to registration component

--- src/app/components/user-register-form/user-register-form.component.ts
+++ src/app/components/user-register-form/user-register-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { FetchApiDataService } from '../../services/fetch-api-data.service'
+
 
 @Component({
   selector: 'app-user-register-form',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-register-form.component.scss']
 })
 export class UserRegisterFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' }
 
-  constructor() { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    // Material dialog uses form compontnet
+    public dialogRef: MatDialogRef<UserRegisterFormComponent>,
+    public snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
   }
 
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+      // successful registration
+      this.dialogRef.close()
+      this.snackBar.open(
+        `${result.username}, you may login.`,
+        'OK', {
+        duration: 5000
+      });
+    }, (result) => {
+      console.log(result)
+      this.snackBar.open(
+        result.username,
+        'OK', {
+        duration: 2000
+      })
+    })
+  }
 }
