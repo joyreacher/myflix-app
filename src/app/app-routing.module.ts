4a4789edad9048a39c25f770bd8dcf013ebd4596
Brian Thomas <btdeveloper125@gmail.com>
2022-01-28 09:30:38 -0700

    feat(Routing): Make Welcom Component the Main Scren

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 import { UserRegisterFormComponent } from './components/user-register-form/user-register-form.component';
 import { UserLoginFormComponent } from './components/user-login-form/user-login-form.component';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
 
 const routes: Routes = [
-  {path: '', component: UserRegisterFormComponent},
-  {path: 'login', component: UserLoginFormComponent}
+  {path: 'welcome', component: WelcomePageComponent},
+  {path: 'movies', component: MovieCardComponent},
+  {path: '', redirectTo: 'welcome', pathMatch: 'prefix'}
 ];
 
 @NgModule({
