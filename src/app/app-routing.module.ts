ca7634f729a56775a602dcec340f22a93e6b5daf
Brian Thomas <btdeveloper125@gmail.com>
2022-01-31 10:01:16 -0700

    feat(User Profile): Add main view route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
-import { MovieCardComponent } from './components/movie-card/movie-card.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+// pages
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 const routes: Routes = [
   {path: 'welcome', component: WelcomePageComponent},
-  {path: 'movies', component: MovieCardComponent},
   {path: 'user', component: UserProfileComponent},
+  {path: 'home', component: MainPageComponent},
   {path: '', redirectTo: 'welcome', pathMatch: 'prefix'}
 ];
 
