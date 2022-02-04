8baf4c3a01d7a61ab910485f6b4fa7f7ff99a33c
Brian Thomas <btdeveloper125@gmail.com>
2022-02-03 18:11:20 -0700

    feat(User Profile): Add delete logic

--- src/app/components/user-delete-form/user-delete-form.component.ts
+++ src/app/components/user-delete-form/user-delete-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { FetchApiDataService } from 'src/app/services/fetch-api-data.service';
 
 @Component({
   selector: 'app-user-delete-form',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-delete-form.component.scss']
 })
 export class UserDeleteFormComponent implements OnInit {
-
-  constructor() { }
+  @Input() userData = { Username: '', Email:''}
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
   }
 
+  deleteUser(username:string, email:string):any{
+    console.log('deleeeeeeete')
+    console.log(username, email)
+    this.fetchApiData.deleteUser({"Username":localStorage.getItem('user'), "Email": email}).subscribe((response)=>{
+      console.log(response)
+      this.snackBar.open(
+        'User was deleted',
+        'OK',{
+          duration:5000
+        }
+      )
+    })
+  }
 }
