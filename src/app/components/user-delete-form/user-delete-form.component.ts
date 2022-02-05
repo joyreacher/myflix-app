750433d336a2d63fd0a4739f449258e640512be9
Brian Thomas <btdeveloper125@gmail.com>
2022-02-05 15:02:20 -0700

    refactor: Update remove user

--- src/app/components/user-delete-form/user-delete-form.component.ts
+++ src/app/components/user-delete-form/user-delete-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { FetchApiDataService } from 'src/app/services/fetch-api-data.service';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-delete-form',
@@ -11,17 +13,20 @@ export class UserDeleteFormComponent implements OnInit {
   @Input() userData = { Username: '', Email:''}
   constructor(
     public fetchApiData: FetchApiDataService,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    public router: Router,
+    public dialog: MatDialog,
   ) { }
 
   ngOnInit(): void {
   }
 
   deleteUser(username:string, email:string):any{
-    console.log('deleeeeeeete')
-    console.log(username, email)
     this.fetchApiData.deleteUser({"Username":localStorage.getItem('user'), "Email": email}).subscribe((response)=>{
-      console.log(response)
+      this.dialog.closeAll()
+      localStorage.removeItem('user')
+      localStorage.removeItem('token')
+      this.router.navigate(['welcome'])
       this.snackBar.open(
         'User was deleted',
         'OK',{
